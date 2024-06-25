844dea2a43e09a9bfc6cb72ce1af84c65aaf7fa8
RickyMandich <ricky.mandich@gmail.com>
2024-06-25 11:53:37 +0200

    aggiornamento 2024 06 25 11:53

--- commit.sh
+++ commit.sh
@@ -1,9 +1,13 @@
 cd "C:\Users\ssppo\Desktop\grest"
 git add .
 git status
+echo press invio to continue...
+read
 nomeCommit=`date "+%Y %m %d %H:%M"`
 nomeCommit="aggiornamento "$nomeCommit
 git commit -m "$nomeCommit"
+read
 clear
 git push
+read
 clear
